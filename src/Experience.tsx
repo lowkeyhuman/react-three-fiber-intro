b46647f0793ad68c3a2cd980be86680f480fdd4b
lowkeyhuman <iamlowkeyhuman@gmail.com>
2023-12-01 09:46:02 +0000

    auto rotate box geometry

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,7 +1,20 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react"
+import { Mesh } from 'three';
+
 const Experience = () => {
+  const boxRef = useRef<Mesh>(null);
+
+  useFrame((_, delta) => {
+    const box = boxRef.current;
+    if (box == null) return;
+
+    box.rotation.y += 0.8 * delta 
+  })
+
   return (
     <>
-      <mesh>
+      <mesh ref={boxRef}>
         <boxGeometry />
         <meshNormalMaterial />
       </mesh>
