af48f20b3119d20ce964a297d63772560e206173
lowkeyhuman <iamlowkeyhuman@gmail.com>
2023-12-01 07:48:33 +0000

    Add basic scene with box

---
+++ src/Experience.tsx
@@ -0,0 +1,14 @@
+const Experience = () => {
+  return (
+    <>
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+
+      <ambientLight />
+    </>
+  )
+}
+
+export default Experience
